b3480b3dca90f4d9a5bc11f85e3af1ceab134a99
9Santi <a.saintandre63@gmail.com>
2023-04-27 10:40:18 +0200

    v2 +modifJS +zipfigma +modifCSS

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
 //curl 'https://geo.api.gouv.fr/communes?nom=Nantes&fields=departement&boost=population&limit=5'
-document.addEventListener("submit", searchByCP);
-document.addEventListener("submit", searchByVille);
+document.addEventListener("input", searchCP);
+document.addEventListener("submit", searchVille);
 
-// Function CP
 
-function searchByCP(event) {
+// CP
+function searchCP(event){
   event.preventDefault();
-  const ValueCP = document.querySelector("#searchCP").value;
-  console.log(ValueCP);
-  const UrlCP = `https://geo.api.gouv.fr/communes?codePostal=${ValueCP}`;
-  console.log(UrlCP);
-  fetch(UrlCP)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
+  const cpInput = document.querySelector("#searchCP").value;
+  const cpUrl = `https://geo.api.gouv.fr/communes?codePostal=${cpInput}`;
+  fetch(cpUrl)
+  .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    const ville = data[0].nom;
-    const code = data[0].codesPostaux;
-    console.log(`Ville: ${ville}, code: ${code}`);
-    document.querySelector("#codepostal").textContent = code;
-    document.querySelector("#logo a").href = `https://www.google.fr/maps/place/${code}+${ville}`;
-    document.querySelector("#ville").textContent = ville;
+      console.log(data);
+      showData(data);
+              // Récupération du nom de la première commune de la liste
+              const nomCommune = data[0].nom;
+
+              // Affichage du nom de la commune dans le champ #ville
+              document.querySelector("#ville").value = nomCommune;
   })
+
   .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
-  });
+    console.error(error);
+    document.querySelector("#ville").textContent = "Non";
+    document.querySelector("#codepostal").textContent = "valide!";
+});
 }
 
-// Function Ville
-
-function searchByVille(event) {
+// Ville
+function searchVille(event){
   event.preventDefault();
-  const ValueVille = document.querySelector("#searchVille").value;
-  console.log(ValueVille);
-  const UrlVille = `https://geo.api.gouv.fr/communes?nom=${ValueVille}`;
-  console.log(UrlVille);
-  fetch(UrlVille)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    const ville = data[0].nom;
-    const code = data[0].codesPostaux;
-    console.log(`Ville: ${ville}, code: ${code}`);
-    document.querySelector("#codepostal").textContent = code;
-    document.querySelector("#logo a").href = `https://www.google.fr/maps/place/${code}+${ville}`;
-    document.querySelector("#ville").textContent = ville;
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
-  });
+  const cityInput = document.querySelector("#searchVille").value;
+  const cityUrl = `https://geo.api.gouv.fr/communes?nom=${cityInput}`;
+  fetch(cityUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showData(data);
+    })
+    .catch((error) => console.error(error));
 }
 
+function showData(data) {
+  const communes = data.map(d => d.nom); // récupère les noms des communes
+  const cp = data[0].codesPostaux; // récupère le premier code postal renvoyé
+  const select = document.createElement("select"); // crée un élément liste déroulante
+  communes.forEach(commune => { // pour chaque commune
+      const option = document.createElement("option"); // crée une option de liste déroulante
+      option.textContent = commune; // ajoute le nom de la commune comme texte
+      select.appendChild(option); // ajoute l'option à l'élément liste déroulante
+  });
+  document.querySelector("#codepostal").innerHTML = ""; // efface le contenu précédent
+  document.querySelector("#ville").innerHTML = "";
+  document.querySelector("#codepostal").textContent = cp; // ajoute le code postal à l'élément résultat
+  document.querySelector("#ville").appendChild(select); // ajoute l'élément liste déroulante à l'élément résultat
+  document.querySelector("#logo a").href = (url=`https://www.google.fr/maps/place/${cp}`);
+}
